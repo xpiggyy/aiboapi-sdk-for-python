e0fca4bdc6a0d1368ad66a70c7d352787f49e51b
xpiggyy <ry875421@gmail.com>
2023-06-16 09:18:12 +0900

    update README.md

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="aiboapi",
-    version="1.0.7",
+    version="1.0.8",
     description="AIBO API SDK for python",
     author="Team 5",
     project_urls={
