222bb97df075e9124b8cb7dab099856f9284144d
xpiggyy <ry875421@gmail.com>
2023-06-16 10:42:53 +0900

    refactoring

--- aiboapi/main.py
+++ aiboapi/main.py
@@ -1,24 +1,87 @@
+import requests
+import json
 from typing import Dict
-from aiboapi.functions.fetch import Functions
+import time
+import sys
+
 
 class Aibo:
     def __init__(self, token) -> None:
-        self.function = Functions(token)
+        self.headers = {
+            "Authorization": f"Bearer {token}",
+            "Content-Type": "application/x-www-form-urlencoded",
+        }
+        response = requests.get(
+            "https://public.api.aibo.com/v1/devices", headers=self.headers
+        )
+        assert response.status_code == requests.codes.ok, sys.exit(
+            "SyntaxError: invalid token!"
+        )
+        response_text = json.loads(response.text)
+        self.device_Id = response_text["devices"][0]["deviceId"]
+        self.nickname = response_text["devices"][0]["nickname"]
 
     def get_devices(self) -> str:
-        device_id = self.function.get_devices()
+        device_id = str(self.device_Id)
         return device_id
 
-    def ask_action(self, API_NAME: str, arguments:str = '{}') -> Dict:
-        """Main method for controlling aibo!!
+    def get_nickname(self) -> str:
+        nickname = str(self.nickname)
+        return nickname
 
-        Args:
-            API_NAME: ask_action(API_NAME = "xxxxxx")
-            arguments (str, optional): _description_. Defaults to None.
+    def send_request(self, API_NAME: str, arguments) -> Dict:
+        headers = self.headers
+        endpoint = (
+            "https://public.api.aibo.com/v1/devices/"
+            + self.device_Id
+            + "/capabilities/"
+            + API_NAME
+            + "/execute"
+        )
+        data = arguments
+        print(data)
+        response = requests.post(endpoint, headers=headers, data=data)
+        response_text = json.loads(response.text)
+        execution_Id = response_text["executionId"]
+        status = response_text["status"]
+        return {"status": status, "execution_Id": execution_Id}
 
-        Returns:
-            str: _description_
-        """
+    def send_get_request(self, execution_Id: str) -> Dict:
+        headers = self.headers
+        endpoint = f"https://public.api.aibo.com/v1/executions/{execution_Id}"
+        response = requests.get(endpoint, headers=headers)
+        response_text = json.loads(response.text)
+        new_execution_Id = response_text["executionId"]
+        new_status = response_text["status"]
+        result = response_text["result"]
+        return {
+            "status": new_status,
+            "execution_Id": new_execution_Id,
+            "result": result,
+        }
 
-        result = self.function.get_result_on_completion(API_NAME, arguments = None)
-        return result
+    def ask_action(self, API_NAME: str, arguments: str = '{}') -> Dict:
+        while True:
+            print('send_request......')
+            send_request_response = self.send_request(API_NAME, arguments)
+            send_request_execution_Id = send_request_response["execution_Id"]
+            send_request_status = send_request_response["status"]
+            if (
+                send_request_status == "ACCEPTED"
+                or send_request_status == "IN_PROGRESS"
+            ):
+                print(send_request_status)
+                print('send_requested!!')
+                break
+            else:
+                print('send_request')
+                time.sleep(1.5)
+        print('send_get_request.....')
+        while True:
+            send_get_request_response = self.send_get_request(send_request_execution_Id)
+            send_get_request_status = send_get_request_response["status"]
+            if send_get_request_status == "SUCCEEDED":
+                return send_get_request_response["result"]
+            else:
+                time.sleep(2.5)
+                print('send_get_request.....')
--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name="aiboapi",
-    version="1.0.8",
+    version="1.0.9",
     description="AIBO API SDK for python",
-    author="Team 5",
+    author="Team 5 belonging to is17",
     project_urls={
         "Documentation": "https://github.com/xpiggyy/aiboapi-sdk-for-python/blob/main/README.md",
         "Source": "https://github.com/xpiggyy/aiboapi-sdk-for-python",
